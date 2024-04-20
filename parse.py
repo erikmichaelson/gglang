53d3249b03012a6372af046770d9d8d9bb1ebf9f
erikmichaelson <47643317+erikmichaelson@users.noreply.github.com>
2024-04-20 12:44:38 -0500

    screwing around with inheiritance

---
+++ parse.py
@@ -0,0 +1,97 @@
+import sys
+
+class Agg_Value():
+    def __init__(self, agg_func, ref):
+        self.agg_func = agg_func
+        self.ref = ref
+
+def split_agg(value):
+    value = value.split('(')
+    assert len(value) == 2
+    assert value[-1][-1] == ')'
+    value[-1] = value[-1][:-1]
+    print(value)
+    return Agg_Value(value[0].upper(), value[1])
+
+class Plot():
+    def __init__(self, plot_type=None, data_source=None):
+        self.plot_type = plot_type
+        self.data_source = data_source
+    def sql(self) -> str:
+        raise "Not implemented"
+
+class Table(Plot):
+    def __init__(self, row=None, col=None, value=None):
+        super().__init__(plot_type = 'TABLE')
+        self.row   = row
+        self.col   = col
+        self.value = value
+        print(f'inside table init, {super().plot_type}')
+    def sql(self):
+        col_values = duckdb.query(f"select distinct {col} from '{data_source}'").fetchall()
+        col_values = [c[0] for c in col_values]
+        print(col_values)
+        value = split_agg(value)
+        ret = f'select {row},\n'
+        for cv in col_values:
+            if value.agg_func == 'COUNT':
+                ret += f"sum(case when {col} = '{cv}' then 1 else 0 end) as '{cv}'"
+            elif value.agg_func == 'SUM':
+                #assert col_type in ['FLOAT', 'NUMERIC', 'INT']
+                ret += f"sum(case when {col} = '{cv}' then {value.ref} else 0 end) as '{cv}'"
+            elif value.agg_func == 'AVG':
+                #assert col_type in ['FLOAT', 'NUMERIC', 'INT']
+                ret += f"avg(case when {col} = '{cv}' then {value.ref} else 0 end) as '{cv}'"
+            elif value.agg_func == 'MAX':
+                ret += f"max(case when {col} = '{cv}' then {value.ref} else 0 end) as '{cv}'"
+            elif value.agg_func == 'MIN':
+                ret += f"min(case when {col} = '{cv}' then {value.ref} else 0 end) as '{cv}'"
+            ret += ',\n' # duckdb is ok with trailing commas
+        ret += f"from '{data_source}' group by {row}"
+        return ret
+
+class Dot(Plot):
+    def __init__(self, x=None, y=None, color=None, size=None):
+        super().__init__(plot_type = 'DOT')
+        self.x = x
+        self.y = y
+        self.color = color
+        self.size  = size
+    def sql(self):
+        ret = 'select {x}, {y},'
+        if color is not None:
+            ret += ' {color}, '
+        if size is not None:
+            ret += ' {size} '
+        ret += 'from {data_source}'
+        return ret
+
+def parse(text: str) -> Plot:
+    text = text.lower()
+    lines = text.split('\n')
+    ret = Plot()
+    for l in lines:
+        l = l.strip().split(' ')
+        print(l)
+        if l[0] == 'table':
+            ret == Table()
+            print(f'table init called, {ret.super().plot_type}')
+        elif l[0] == 'dot':
+            ret == Dot()
+        elif l[0] == 'row':
+            assert ret.plot_type == 'TABLE', f'plot type = {ret.plot_type}'
+            ret.row = l[1]
+        elif l[0] == 'col':
+            assert ret.plot_type == 'TABLE', f'plot type = {ret.plot_type}'
+            ret.col = l[1]
+        elif l[0] == 'x':
+            assert ret.plot_type == 'DOT'
+            ret.x = l[1]
+        elif l[0] == 'y':
+            assert ret.plot_type == 'DOT'
+            ret.y = l[1]
+    print(__dir__(ret))
+    return ret
+
+if __name__ == '__main__':
+    parse(open(sys.argv[1]).read())
